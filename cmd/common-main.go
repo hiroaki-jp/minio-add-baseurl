0e78f0686c40e0d29c62fc20a58ce61c6942b8dd
hiroaki <207416720+hiroaki-jp@users.noreply.github.com>
2025-12-12 01:32:42 +0900

    bug fix

--- cmd/common-main.go
+++ cmd/common-main.go
@@ -120,18 +120,22 @@ const consolePrefix = "CONSOLE_"
 func minioConfigToConsoleFeatures() {
 	os.Setenv("CONSOLE_PBKDF_SALT", globalDeploymentID())
 	os.Setenv("CONSOLE_PBKDF_PASSPHRASE", globalDeploymentID())
+
+	var minioServerURL string
 	if globalMinioEndpoint != "" {
-		os.Setenv("CONSOLE_MINIO_SERVER", globalMinioEndpoint)
+		minioServerURL = globalMinioEndpoint
 	} else {
 		// Explicitly set 127.0.0.1 so Console will automatically bypass TLS verification to the local S3 API.
 		// This will save users from providing a certificate with IP or FQDN SAN that points to the local host.
-		minioServerURL := fmt.Sprintf("%s://127.0.0.1:%s", getURLScheme(globalIsTLS), globalMinioPort)
-		// If API base path is configured, append it to the MinIO server URL
-		if globalAPIBasePath != "" {
-			minioServerURL += globalAPIBasePath
-		}
-		os.Setenv("CONSOLE_MINIO_SERVER", minioServerURL)
+		minioServerURL = fmt.Sprintf("%s://127.0.0.1:%s", getURLScheme(globalIsTLS), globalMinioPort)
 	}
+
+	// If API base path is configured, append it to the MinIO server URL
+	// (unless it's already included in globalMinioEndpoint)
+	if globalAPIBasePath != "" && !strings.HasSuffix(minioServerURL, globalAPIBasePath) {
+		minioServerURL += globalAPIBasePath
+	}
+	os.Setenv("CONSOLE_MINIO_SERVER", minioServerURL)
 	if value := env.Get(config.EnvMinIOLogQueryURL, ""); value != "" {
 		os.Setenv("CONSOLE_LOG_QUERY_URL", value)
 		if value := env.Get(config.EnvMinIOLogQueryAuthToken, ""); value != "" {
